02b5001777d2d89a4c22ce9756997a1ef5cd2117
Wiktor <wiktor.rostkowski@hotmail.com>
2024-03-26 00:46:59 +0100

    Add Custom Calendar Library

--- app/src/main/java/pl/edu/pjwstk/pamo/schedule/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/pl/edu/pjwstk/pamo/schedule/ui/calendar/CalendarFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.ui.text.TextStyle
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.view.CalendarView
 import com.kizitonwose.calendar.view.MonthDayBinder
 import pl.edu.pjwstk.pamo.schedule.databinding.FragmentCalendarBinding
 import pl.edu.pjwstk.pamo.schedule.R
 import java.time.YearMonth
+import java.util.Locale
 
 class CalendarFragment : Fragment() {
         override fun onCreateView(
@@ -23,16 +27,29 @@ class CalendarFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_calendar, container, false)
 
-        val calendarView = root.findViewById<CalendarView>(R.id.calendarView)
+        val calendarView = root.findViewById<CalendarView>(R.id.weekCalendarView)
         val dayViewResource = R.layout.calendar_day_layout
 
         // calendar config
         val currentMonth = YearMonth.now()
         val startMonth = currentMonth.minusMonths(100)
         val endMonth = currentMonth.plusMonths(100)
-        val firstDayOfWeek = firstDayOfWeekFromLocale()
-        calendarView.setup(startMonth, endMonth, firstDayOfWeek)
-        calendarView.scrollToMonth(currentMonth)
+            val firstDayOfWeek = firstDayOfWeekFromLocale()
+            calendarView.setup(startDate, endDate, firstDayOfWeek)
+            calendarView.scrollToWeek(currentDate)
+            val daysOfWeek = daysOfWeek()
+
+            val titlesContainer = root.findViewById<ViewGroup>(R.id.titlesContainer)
+            titlesContainer.children
+                .map { it as TextView }
+                .forEachIndexed { index, textView ->
+                    val daysOfWeek
+                    val dayOfWeek = daysOfWeek[index]
+                    val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                    textView.text = title
+                }
+
+
 
 
         calendarView.dayBinder = object : MonthDayBinder<CalendarViewModel> {
