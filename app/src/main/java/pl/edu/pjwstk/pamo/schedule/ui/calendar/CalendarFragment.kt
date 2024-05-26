6bef3d4d7692d4fd7d7ad7a0bade642dd79ec146
Wiktor <wiktor.rostkowski@hotmail.com>
2024-05-26 23:01:30 +0200

    Calendar Update

--- app/src/main/java/pl/edu/pjwstk/pamo/schedule/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/pl/edu/pjwstk/pamo/schedule/ui/calendar/CalendarFragment.kt
@@ -1,69 +1,130 @@
 package pl.edu.pjwstk.pamo.schedule.ui.calendar
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
-import com.kizitonwose.calendar.view.CalendarView
+import com.kizitonwose.calendar.core.*
 import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.ViewContainer
 import pl.edu.pjwstk.pamo.schedule.R
+import pl.edu.pjwstk.pamo.schedule.databinding.CalendarDayLayoutBinding
+import pl.edu.pjwstk.pamo.schedule.databinding.CalendarHeaderBinding
+import pl.edu.pjwstk.pamo.schedule.databinding.FragmentCalendarBinding
+import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
-
-class DayViewContainer(view: View) : ViewContainer(view) {
-    val textView = view.findViewById<TextView>(R.id.calendarDayText)
-    // Will be set when this container is bound
-    lateinit var day: CalendarDay
-
-    init {
-        view.setOnClickListener {
-            Log.w("DAY CLICK", day.date.toString())
-
-            val bundle = Bundle()
-            bundle.putString("date", day.date.format(DateTimeFormatter.ISO_LOCAL_DATE))
-
-            Navigation.findNavController(view).navigate(R.id.navigation_today, bundle)
-        }
-    }
-}
-
 class CalendarFragment : Fragment() {
-        override fun onCreateView(
+    private var selectedDate: LocalDate? = null
+    private val today = LocalDate.now()
+
+    private lateinit var binding: FragmentCalendarBinding
+
+
+    override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_calendar, container, false)
+    ): View {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        val calendarView = root.findViewById<CalendarView>(R.id.calendarView)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // calendar config
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(100)
-        val endMonth = currentMonth.plusMonths(100)
-        val firstDayOfWeek = firstDayOfWeekFromLocale()
-        calendarView.setup(startMonth, endMonth, firstDayOfWeek)
-        calendarView.scrollToMonth(currentMonth)
-
-
-        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            override fun create(view: View) = DayViewContainer(view)
-
-            override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.day = data
-                container.textView.text = data.date.dayOfMonth.toString()
+        val daysOfWeek = daysOfWeek()
+        binding.legendLayout.root.children.forEachIndexed { index, child ->
+            (child as TextView).apply {
+                text = daysOfWeek[index].name.first().toString()
+                setTextColor(resources.getColor(R.color.example_2_white, null))
             }
         }
 
-        return root
+        configureBinders()
+        binding.calendarView.setup(
+            YearMonth.now(),
+            YearMonth.now().plusMonths(100),
+            daysOfWeek.first(),
+        )
+    }
+
+
+
+    private fun configureBinders() {
+        val calendarView = binding.calendarView
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay
+            val textView = CalendarDayLayoutBinding.bind(view).exTwoDayText
+
+            init {
+                textView.setOnClickListener {
+/*                    if (day.position == DayPosition.MonthDate) {
+                        if (selectedDate == day.date) {
+                            selectedDate = null
+                            calendarView.notifyDayChanged(day)
+                        } else {
+                            val oldDate = selectedDate
+                            selectedDate = day.date
+                            calendarView.notifyDateChanged(day.date)
+                            oldDate?.let { calendarView.notifyDateChanged(oldDate) }
+                        }
+                        menuItem.isVisible = selectedDate != null
+                    }*/
+                    Log.w("DAY CLICK", day.date.toString())
+
+                    val bundle = Bundle()
+                    bundle.putString("date", day.date.format(DateTimeFormatter.ISO_LOCAL_DATE))
+
+                    Navigation.findNavController(view).navigate(R.id.navigation_today, bundle)
+                }
+            }
+        }
+
+        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                container.day = data
+                val textView = container.textView
+                textView.text = data.date.dayOfMonth.toString()
+
+                if (data.position == DayPosition.MonthDate) {
+                    textView.visibility = View.VISIBLE
+                    when (data.date) {
+                        selectedDate -> {
+                            textView.setTextColor(resources.getColor(R.color.example_2_white, null))
+                            textView.setBackgroundResource(R.drawable.example_2_selected_bg)
+                        }
+                        today -> {
+                            textView.setTextColor(resources.getColor(R.color.example_2_red, null))
+                            textView.background = null
+                        }
+                        else -> {
+                            textView.setTextColor(resources.getColor(R.color.example_2_black, null))
+                            textView.background = null
+                        }
+                    }
+                } else {
+                    textView.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val textView = CalendarHeaderBinding.bind(view).exTwoHeaderText
+        }
+
+        calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, data: CalendarMonth) {
+                container.textView.text = data.yearMonth.toString()
+            }
+        }
     }
 }
