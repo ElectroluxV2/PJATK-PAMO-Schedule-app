10a9891e62eb1ffdc1a5849185291f95344a5ffb
Wiktor <wiktor.rostkowski@hotmail.com>
2024-06-01 23:48:05 +0200

    Add expresso Test with Keydoc description

---
+++ app/src/androidTest/java/pl/edu/pjwstk/pamo/schedule/CheckReloadTest.kt
@@ -0,0 +1,110 @@
+package pl.edu.pjwstk.pamo.schedule
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+/**
+ * [CheckReloadTest] verifies the functionality of the settings view and the reload data button.
+ *
+ * The test performs the following steps:
+ * 1. Clicks on the settings icon in the bottom navigation bar to open the settings view.
+ * 2. Verifies that the "Reload data" button is displayed.
+ * 3. Clicks on the "Reload data" button.
+ * 4. Checks if the logs text view is displayed after clicking the "Reload data" button.
+ */
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CheckReloadTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest2() {
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_settings), withContentDescription("Settings"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val button = onView(
+            allOf(
+                withId(R.id.loadButton), withText("Reload data"),
+                withParent(withParent(withId(R.id.nav_host_fragment_activity_main))),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.loadButton), withText("Reload data"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment_activity_main),
+                        0
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.logs),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
